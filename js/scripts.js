f3d7b515228d444bbee5626564aea200972eaca7
Misha Petrov and Stephanie Faber <misha@>
2018-01-23 09:55:50 -0800

    add js logic to calculate price

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+function Ticket(title, time, age) {
+  this.movie = title;
+  this.time = time;
+  this.age = age;
+  var price = 10; // Standard price of $10
+  if (isAgeDiscount(age) === true) {
+    price = price - 1.5;
+  };
+  if (isOldTitleDiscount(title) === true) {
+    price = price - 1.5;
+  };
+  if (isShowingTimeDiscount(time) === true) {
+    price = price - 1.5;
+  };
+  this.price = price;
+};
+
+var isAgeDiscount = function(age) {
+  var year = age.slice(0, 4);
+  var currentYear = 2018;
+  var ageOfUser = currentYear - year;
+  if (ageOfUser <= 10 || ageOfUser >= 55) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+var isOldTitleDiscount = function(movie) {
+  if (movie === "old") {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+var isShowingTimeDiscount = function(showing) {
+  if (showing === "morning") {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+$(document).ready(function() {
+  $("form#tickets-form").submit(function(event) {
+    event.preventDefault();
+    var movie = $("#movies").val();
+    var showing = $("#showing").val();
+    var age = $("input#age").val();
+    var newTicket = new Ticket(movie, showing, age);
+    $(".price").text(newTicket.price);
+  });
+});
